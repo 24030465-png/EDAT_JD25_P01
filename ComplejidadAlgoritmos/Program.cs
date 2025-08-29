a2ce8606724e9ebde197ef1e7b117999e1c72abe
AKI <24030465@itesa.edu.mx>
2025-08-29 14:42:26 -0600

    version_03: Complejidad de algoritmos

---
+++ ComplejidadAlgoritmos/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("complejidad de algoritmos\n");
+        Console.WriteLine("complejidad Big o(n^2)");
+        //Console.WriteLine("complejidad Big o(n)");
+
+        //var numeros= new List<int>() {5,2,3,200,2000,10000,0,1 };
+        //var numeroMayor = (List<int> arreglo) => 
+        //{
+        //    if (arreglo == null || arreglo.Count == 0)
+        //    {
+        //        return 0;
+        //    }
+        //        int mayor = arreglo[0];
+        //        for (int i = 0; i < arreglo.Count; i++)
+        //        {
+        //            if (arreglo[i] > mayor)
+        //            {
+        //                mayor=arreglo[i];
+        //            }
+        //        } 
+        //    return mayor;
+        //};
+        //int may=numeroMayor(numeros);
+        //Console.WriteLine("El numero mayor es: "+may);
+
+
+
+
+        //Console.WriteLine("complejidad Big o(1)");
+
+        //var nombres = new List<string>() { "Ileny", "Alondra", "Jose", "Juan", "Alejandro", "Karla" };
+        //var primero = (List<string> arreglo)=> arreglo!= null && arreglo.Count>0? 
+        //    arreglo[0] : "Lista vacia";
+        //Console.WriteLine(primero(nombres));
+        Console.ReadKey();  
+    }
+}
